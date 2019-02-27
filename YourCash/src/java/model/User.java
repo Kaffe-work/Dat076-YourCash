1b0e2600964b182ddbb6b5cfac5d270fde8882a1
RemainTheSame <halooscur.orava@gmail.com>
2019-02-27 13:28:23 +0100

    Get/Set methods added to User

--- YourCash/src/java/model/User.java
+++ YourCash/src/java/model/User.java
@@ -21,5 +21,29 @@ public class User {
         this.password = password;
         this.id = id;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
     
 }
