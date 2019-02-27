e6ff2bde31ced4f56e979d5089a56f758a573978
RemainTheSame <halooscur.orava@gmail.com>
2019-02-27 13:44:05 +0100

    UserRegistry for handling users

---
+++ YourCash/src/java/model/UserRegistry.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author oscar
+ */
+public class UserRegistry {
+    
+    private List<User> userList = new ArrayList();
+    
+    public UserRegistry(){
+    
+    }
+    
+    public boolean register(){
+         
+        return false;
+    }
+    
+    public User find(){
+        
+        return null;
+    }
+    
+}
