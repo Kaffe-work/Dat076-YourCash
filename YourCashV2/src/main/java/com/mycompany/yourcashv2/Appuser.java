ac8c12d2b43bcafd1f373f6277b9cd7b849917c7
RemainTheSame <34090420+RemainTheSame@users.noreply.github.com>
2019-03-07 15:56:40 +0100

    Initial V2

---
+++ YourCashV2/src/main/java/com/mycompany/yourcashv2/Appuser.java
@@ -0,0 +1,104 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.yourcashv2;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author Oscar Orava Kilberg
+ */
+@Entity
+@Table(name = "appuser")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Appuser.findAll", query = "SELECT a FROM Appuser a")
+    , @NamedQuery(name = "Appuser.findByUsername", query = "SELECT a FROM Appuser a WHERE a.username = :username")
+    , @NamedQuery(name = "Appuser.findByName", query = "SELECT a FROM Appuser a WHERE a.name = :name")
+    , @NamedQuery(name = "Appuser.findByPasword", query = "SELECT a FROM Appuser a WHERE a.pasword = :pasword")})
+public class Appuser implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 2147483647)
+    @Column(name = "username")
+    private String username;
+    @Size(max = 2147483647)
+    @Column(name = "name")
+    private String name;
+    @Size(max = 2147483647)
+    @Column(name = "pasword")
+    private String pasword;
+
+    public Appuser() {
+    }
+
+    public Appuser(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPasword() {
+        return pasword;
+    }
+
+    public void setPasword(String pasword) {
+        this.pasword = pasword;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (username != null ? username.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Appuser)) {
+            return false;
+        }
+        Appuser other = (Appuser) object;
+        if ((this.username == null && other.username != null) || (this.username != null && !this.username.equals(other.username))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.mycompany.yourcashv2.Appuser[ username=" + username + " ]";
+    }
+    
+}
---
+++ YourCashV2/src/main/java/com/mycompany/yourcashv2/Expense.java
@@ -0,0 +1,131 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.yourcashv2;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author Oscar Orava Kilberg
+ */
+@Entity
+@Table(name = "expense")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Expense.findAll", query = "SELECT e FROM Expense e")
+    , @NamedQuery(name = "Expense.findById", query = "SELECT e FROM Expense e WHERE e.id = :id")
+    , @NamedQuery(name = "Expense.findByCost", query = "SELECT e FROM Expense e WHERE e.cost = :cost")
+    , @NamedQuery(name = "Expense.findByTimedate", query = "SELECT e FROM Expense e WHERE e.timedate = :timedate")
+    , @NamedQuery(name = "Expense.findByCategory", query = "SELECT e FROM Expense e WHERE e.category = :category")})
+public class Expense implements Serializable {
+
+    @Size(max = 2147483647)
+    @Column(name = "description")
+    private String description;
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 2147483647)
+    @Column(name = "id")
+    private String id;
+    // @Max(value=?)  @Min(value=?)//if you know range of your decimal fields consider using these annotations to enforce field validation
+    @Column(name = "cost")
+    private Float cost;
+    @Column(name = "timedate")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date timedate;
+    @Size(max = 2147483647)
+    @Column(name = "category")
+    private String category;
+
+    public Expense() {
+    }
+
+    public Expense(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Float getCost() {
+        return cost;
+    }
+
+    public void setCost(Float cost) {
+        this.cost = cost;
+    }
+
+    public Date getTimedate() {
+        return timedate;
+    }
+
+    public void setTimedate(Date timedate) {
+        this.timedate = timedate;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Expense)) {
+            return false;
+        }
+        Expense other = (Expense) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.mycompany.yourcashv2.Expense[ id=" + id + " ]";
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
+}
